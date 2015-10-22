1c108460ff44f2bbf24ee8aa783d38284010cf6f
JeffOwOSun <sunyushi9546@gmail.com>
2015-10-22 21:20:32 +0800

    Adapt todoCtrl.js for new backend

--- js/controllers/todoCtrl.js
+++ js/controllers/todoCtrl.js
@@ -7,8 +7,8 @@
 * - exposes the model to the template and provides event handlers
 */
 todomvc.controller('TodoCtrl',
-['$scope', '$location', '$firebaseArray', '$sce', '$localStorage', '$window',
-function ($scope, $location, $firebaseArray, $sce, $localStorage, $window) {
+['$scope', '$location', '$http', '$sce', '$localStorage', '$window',
+function ($scope, $location, $http, $sce, $localStorage, $window) {
 	// set local storage
 	$scope.$storage = $localStorage;
 
@@ -33,15 +33,10 @@ if (!roomId || roomId.length === 0) {
 // TODO: Please change this URL for your app
 var firebaseURL = "https://classquestion.firebaseio.com/";
 
-
 $scope.roomId = roomId;
-var url = firebaseURL + roomId + "/questions/";
-var echoRef = new Firebase(url);
 
-var query = echoRef.orderByChild("order");
 // Should we limit?
 //.limitToFirst(1000);
-$scope.todos = $firebaseArray(query);
 
 //$scope.input.wholeMsg = '';
 $scope.editedTodo = null;
@@ -60,12 +55,6 @@ $scope.$watchCollection('todos', function () {
 		if (todo.completed === false) {
 			remaining++;
 		}
-
-		// set time
-		todo.dateString = new Date(todo.timestamp).toString();
-		todo.tags = todo.wholeMsg.match(/#\w+/g);
-
-		todo.trustedDesc = $sce.trustAsHtml(todo.linkedDesc);
 	});
 
 	$scope.totalCount = total;
@@ -75,36 +64,28 @@ $scope.$watchCollection('todos', function () {
 	$scope.absurl = $location.absUrl();
 }, true);
 
-// Get the first sentence and rest
-$scope.getFirstAndRestSentence = function($string) {
-	var head = $string;
-	var desc = "";
-
-	var separators = [". ", "? ", "! ", '\n'];
-
-	var firstIndex = -1;
-	for (var i in separators) {
-		var index = $string.indexOf(separators[i]);
-		if (index == -1) continue;
-		if (firstIndex == -1) {firstIndex = index; continue;}
-		if (firstIndex > index) {firstIndex = index;}
-	}
-
-	if (firstIndex !=-1) {
-		head = $string.slice(0, firstIndex+1);
-		desc = $string.slice(firstIndex+1);
-	}
-	return [head, desc];
-};
-
 $scope.addTodo = function () {
 	var newTodo = $scope.input.wholeMsg.trim();
 
 	// No input, so just do nothing
 	if (!newTodo.length) {
 		return;
-	}
+	};
+	$http.post('/api/questions', {wholeMsg: $scope.input.wholeMsg})
+	.success(function(data) {
+		// remove the posted question in the input
+		$scope.input.wholeMsg = '';
+        $scope.todos = data;
+        console.log(data);
+    })
+    .error(function(data) {
+        console.log('Error: ' + data);
+    });
 
+
+
+	//sanitize the input
+/*OLD fronend logic. They have been implemented on backend now.
 	var firstAndLast = $scope.getFirstAndRestSentence(newTodo);
 	var head = firstAndLast[0];
 	var desc = firstAndLast[1];
@@ -121,8 +102,7 @@ $scope.addTodo = function () {
 		echo: 0,
 		order: 0
 	});
-	// remove the posted question in the input
-	$scope.input.wholeMsg = '';
+	*/
 };
 
 $scope.editTodo = function (todo) {
