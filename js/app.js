5994d83b101663519c3dc59772c3415847566fd9
bdengNE <hkustdbw@gmail.com>
2015-11-01 02:14:10 +0800

    delete post

--- js/app.js
+++ js/app.js
@@ -7,4 +7,13 @@
 *
 * @type {angular.Module}
 */
-var todomvc = angular.module('todomvc', ['firebase', 'ngStorage']);
+var todomvc = angular.module('todomvc', [ 'ngStorage']);
+
+/*
+angular.module('todomvc').config(function($locationProvider){
+    $locationProvider.html5Mode(true).hashPrefix('!');
+});
+angular.module('todomvc').run(function($FB){
+  $FB.init('YOUR_APPID');
+});
+*/
